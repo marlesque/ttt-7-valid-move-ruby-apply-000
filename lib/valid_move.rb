44fab02f04b488a93b254c423822b00223d4d7f3
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 17:01:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
-def valid_move()
-  if 
+def valid_move(board, index)
+  if (board[index]=="" || board[index]==" " || board[index]==nil)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken?()
+def position_taken?(board, index)
 
 end
